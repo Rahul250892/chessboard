7a83de9111021258f03cac1c6bb2a99b117c335c
rahul250892 <rahulkumar250892@outlook.com>
2020-09-09 16:31:10 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+//import ChessApp from './chess/ChessApp';
+import BoardRootContainer from './chessboard/root/BoardRootContainer';
+
+function App() {
+  return (
+     <BoardRootContainer />
+    
+  );
+}
+
+export default App;
